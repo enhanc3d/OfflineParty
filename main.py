53a2823282a6460c245c273ff014f924131ecac2
2000GHz <59731892+2000GHz@users.noreply.github.com>
2023-05-23 20:54:14 +0200

    Function improvements and multithreading downloads

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import json
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, unquote, urlparse, urlunparse
 from pathvalidate import sanitize_filename
+from concurrent.futures import ThreadPoolExecutor
 
 
 def clear_console():
@@ -20,44 +21,59 @@ def make_soup(url: str) -> BeautifulSoup:
     return soup
 
 
+def get_next_page(artist_page):
+    response = requests.get(artist_page)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    next_link = soup.find('a', class_='next')
+
+    if next_link is not None:
+        href = next_link.get('href')
+        next_url = urljoin(artist_page, href)
+        print(next_url, "= next URL")
+        return next_url
+
+    return None
+
+
 def get_new_posts(artist_id, artist_page):
     soup = make_soup(artist_page)
-    artist_uploaded = False
     artist_info = get_artist_info(soup, artist_page)
-
     artist_total_posts = artist_info['number_of_posts']
 
     # If the JSON file does not exist or is empty, return the current total posts
     if not os.path.exists("latest_post_data.json") or os.stat("latest_post_data.json").st_size == 0:
-        # Create an empty JSON file with '{}' as the initial content
         with open("latest_post_data.json", 'w') as f:
-            f.write('{}')
-        return artist_total_posts, artist_uploaded
+            f.write(json.dumps({artist_id: {'number_of_posts': artist_total_posts}}))
+        return artist_total_posts, False
 
     with open("latest_post_data.json", 'r') as f:
         data = json.load(f)
 
     # If the artist is not in the JSON file, return the current total posts
     if artist_id not in data:
-        return artist_total_posts, artist_uploaded
+        return artist_total_posts, False
 
     downloaded_posts = data[artist_id].get('number_of_posts', 0)
-    print(downloaded_posts, "downloaded posts")
-
     new_posts = artist_total_posts - downloaded_posts
-    print(new_posts, "new posts")
 
     if new_posts != 0:
         artist_uploaded = True
+        data[artist_id]['number_of_posts'] = artist_total_posts
+
+        with open("latest_post_data.json", 'w') as f:
+            json.dump(data, f, indent=4)
+    else:
+        artist_uploaded = False
 
     return new_posts, artist_uploaded
+
 
 
 # JSON MANAGEMENT FUNCTIONS #
 
 def save_latest_post_data(artist: str, id: int, date: str, number_of_posts: int, 
                           last_downloaded_post: str, downloaded_posts: int, first_post: bool):
-    
+
     all_data = {}
 
     # If the JSON file already exists, load its current contents
@@ -88,7 +104,6 @@ def save_latest_post_data(artist: str, id: int, date: str, number_of_posts: int,
     # Save the new JSON data with proper encoding
     with open('latest_post_data.json', 'w', encoding='utf-8') as json_file:
         json.dump(all_data, json_file, indent=4, ensure_ascii=False)  # Set ensure_ascii=False
-
 
 
 def load_latest_post_data(artist: str):
@@ -230,27 +245,30 @@ def fetch_post_media(url: str, artist_folder: str):
     os.makedirs(folder, exist_ok=True)  # Ensure the folder is created before anything else
 
     media_tags = post_info['media_tags']
-    for media_tag in media_tags:
-        media_url = urljoin(url, media_tag.get('href'))
-        media_name = media_url.split('/')[-1].split('?')[0]
-        media_name = sanitize_filename(unquote(media_name))
-        try:
-            print(f'Downloading {media_name}')
-            download(media_url, media_name, folder)
-        except Exception as e:
-            print(f'Error occurred while downloading {media_name}: {str(e)}')
-            write_error_to_file(folder, media_name, url, str(e))
-
     download_tags = post_info['download_tags']
-    for download_tag in download_tags:
-        download_url = urljoin(url, download_tag.get('href'))
-        download_name = download_url.split('/')[-1].split('?')[0]
-        download_name = sanitize_filename(unquote(download_name))
-        try:
-            print(f'Downloading {download_name}')
-            download(download_url, download_name, folder)
-        except Exception as error:
-            write_error_to_file(folder, download_name, url, str(error))
+
+    # Create a ThreadPoolExecutor to manage the download tasks
+    with ThreadPoolExecutor() as executor:
+        # Download media files
+        for media_tag in media_tags:
+            media_url = urljoin(url, media_tag.get('href'))
+            media_name = media_url.split('/')[-1].split('?')[0]
+            media_name = sanitize_filename(unquote(media_name))
+            try:
+                executor.submit(download, media_url, media_name, folder)
+            except Exception as e:
+                print(f'Error occurred while downloading {media_name}: {str(e)}')
+                write_error_to_file(folder, media_name, url, str(e))
+
+        # Download attached files
+        for download_tag in download_tags:
+            download_url = urljoin(url, download_tag.get('href'))
+            download_name = download_url.split('/')[-1].split('?')[0]
+            download_name = sanitize_filename(unquote(download_name))
+            try:
+                executor.submit(download, download_url, download_name, folder)
+            except Exception as error:
+                write_error_to_file(folder, download_name, url, str(error))
 
     # Content text from the posts gets saved to a .txt file
     content_text = post_info['post_content']
@@ -280,11 +298,10 @@ def download(url: str, filename: str, folder):
 
 
 def scrape_artist_page(artist_page):
-    soup = make_soup(artist_page)
-
     parsed_url = urlparse(artist_page)
     artist_page = urlunparse(parsed_url._replace(query=''))
 
+    soup = make_soup(artist_page)
     artist_info = get_artist_info(soup, artist_page)
 
     artist_name = artist_info['artist_name']
@@ -310,7 +327,7 @@ def scrape_artist_page(artist_page):
             print('No posts found for this artist.')
             return
 
-       # Load the saved latest post id and downloaded_posts count
+        # Load the saved latest post id and downloaded_posts count
         latest_post_id, downloaded_posts = load_latest_post_data(artist_id)
 
         new_post_urls = []
@@ -329,7 +346,7 @@ def scrape_artist_page(artist_page):
             elif post_id <= latest_post_id:
                 break
 
-        # Get the number of new posts and if artist uploaded any content
+        # Get the number of new posts and if the artist uploaded any content
         number_of_new_posts, artist_uploaded = get_new_posts(artist_id, artist_page)
 
         if number_of_new_posts > 0:
@@ -347,7 +364,10 @@ def scrape_artist_page(artist_page):
 
         # Bool flag to check if it is the first post
         # Runs once in the first loop
-        first_post = True
+        # Check if the URL format indicates the first page
+        first_post = False
+        if parsed_url.query == 'o=0' or not parsed_url.query:
+            first_post = True
 
         for i, post_url in enumerate(new_post_urls):
             if i > 0:
@@ -371,29 +391,35 @@ def scrape_artist_page(artist_page):
                 if first_post:
                     # Save the latest post data
                     save_latest_post_data(artist_id,
-                              post_id,
-                              post_date,
-                              number_of_posts,
-                              post_url,
-                              downloaded_posts + i + 1,
-                              first_post)
+                                          post_id,
+                                          post_date,
+                                          number_of_posts,
+                                          post_url,
+                                          downloaded_posts + i + 1,
+                                          first_post)
                     first_post = False
                 else:
                     # Update only post_url and downloaded_posts
                     save_latest_post_data(artist_id,
-                              post_id,
-                              post_date,
-                              number_of_posts,
-                              post_url,
-                              downloaded_posts + i + 1,
-                              first_post)
+                                          post_id,
+                                          post_date,
+                                          number_of_posts,
+                                          post_url,
+                                          downloaded_posts + i + 1,
+                                          first_post)
             except Exception as e:
                 print(f"Exception occurred while fetching media for post {post_url}: {e}")
                 break
 
             print(f'Finished downloading post {i+1} of {number_of_new_posts}.')
 
-    print('Finished scraping artist page.')
+        print('Finished scraping artist page.')
+
+        next_page = get_next_page(artist_page)
+        if next_page:
+            scrape_artist_page(next_page)
+        else:
+            print('All pages scraped.')
 
 
 if __name__ == '__main__':
