5b907d2c1bb0893f097c290742d737879acce7f7
2000GHz <59731892+2000GHz@users.noreply.github.com>
2023-09-27 15:48:29 +0200

    Added installation scripts

---
+++ install_macos.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+# Check if Python is installed
+if ! command -v python3 &> /dev/null
+then
+    echo "Python is not found. Installing via Homebrew..."
+    brew install python3
+else
+    echo "Python is already installed."
+fi
+
+# Check and install missing packages
+while IFS= read -r requirement; do
+    # Skip empty lines
+    if [ -z "$requirement" ]; then
+        continue
+    fi
+    
+    python3 -m pip show "$requirement" &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Installing missing package: $requirement"
+        python3 -m pip install "$requirement"
+    else
+        echo "$requirement is already installed."
+    fi
+done < requirements.txt
+
+echo "Done! Press any key to exit."
+read -n 1 -s -r
