56ea7cf52a3ecc6277dda8d91cc333776c8198d2
2000GHz <59731892+2000GHz@users.noreply.github.com>
2023-09-29 00:14:54 +0200

    Added new function to generate the necessary json dictionaries

--- user_search.py
+++ user_search.py
@@ -4,6 +4,49 @@ import get_favorites
 from bs4 import BeautifulSoup
 import requests
 
+
+def generate_json_dictionary_from_data(url):
+    # Define regular expressions for extracting domain, service, and id
+    domain_pattern = r"https://(.*?)/"
+    service_pattern = r"/([^/]+)/user/"
+    id_pattern = r"user/([^?]+)"
+
+    # Extract domain, service, and id using regular expressions
+    domain_match = re.search(domain_pattern, url)
+    service_match = re.search(service_pattern, url)
+    id_match = re.search(id_pattern, url)
+
+    if domain_match and service_match and id_match:
+        domain = domain_match.group(1)
+        service = service_match.group(1)
+        artist_id = id_match.group(1)
+
+        # Initialize username as None
+        username = None
+
+        if "coomer" in domain:
+            username = artist_id
+        else:
+            # Load the website and parse HTML to find the username
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                meta_tag = soup.find('meta', attrs={'name': 'artist_name', 'content': True})
+                if meta_tag:
+                    username = meta_tag['content']
+        return {
+            "faved_seq": "UNKNOWN",
+            "id": username if username else "UNKNOWN",
+            "indexed": "UNKNOWN",
+            "last_imported": "UNKNOWN",
+            "name": username if username else "UNKNOWN",
+            "service": service,
+            "updated": "UNKNOWN"
+        }
+    else:
+        return None
+
+
 # Define a flag to indicate whether the URL has been found
 url_found = False
 
@@ -24,7 +67,7 @@ def input_and_transform_url():
         if re.match(input_expected_pattern, input_url):
             # Transform the input URL
             url_parts = input_url.split('/')
-            transformed_url = '/'.join(url_parts[:6])
+            transformed_url = f"https://{url_parts[2]}/api/{url_parts[3]}/user/{url_parts[5]}"
             # debug transformed url -- print(transformed_url)
             return transformed_url
         else:
@@ -104,6 +147,7 @@ def main(username):
 
         # Set the flag to indicate URL found and exit the function
         url_found = True
+
         return get_list_of_user_urls(domain, service, artist_id, url), username, json_file_path
 
     else:
@@ -132,17 +176,17 @@ def main(username):
                     id_value = found_user.get("id")
                     if id_value.isdigit():
                         domain = "kemono.party"
-                        json_file_path = kemono_json_file_path
                     else:
                         domain = "coomer.party"
-                        json_file_path = coomer_json_file_path
                     url = f"https://{domain}/api/{found_user.get('service')}/user/{found_user.get('id')}"
                     service = found_user.get('service')
                     artist_id = found_user.get('id')
                     # debug found url -- print(url)
                     # Set the flag to indicate URL found and exit the function
-                    url_found = True              
-                    return get_list_of_user_urls(domain, service, artist_id, url), username, json_file_path
+                    url_found = True
+                    json_data = generate_json_dictionary_from_data(url)
+                    return_data = get_list_of_user_urls(domain, service, artist_id, url), username, json_data
+                    return return_data
 
             transformed_input_url = input_and_transform_url()
 
@@ -166,11 +210,8 @@ def main(username):
                     username = None
 
                     if "coomer" in domain:
-                        json_file_path = coomer_json_file_path
-                        json_file_path = coomer_json_file_path
                         username = artist_id
                     else:
-                        json_file_path = kemono_json_file_path
                         # Load the website and parse HTML to find the username
                         response = requests.get(url)
                         if response.status_code == 200:
@@ -178,10 +219,11 @@ def main(username):
                             meta_tag = soup.find('meta', attrs={'name': 'artist_name', 'content': True})
                             if meta_tag:
                                 username = meta_tag['content']
-                    return domain, service, artist_id, username, json_file_path
+                    return domain, service, artist_id, username
 
-            domain, service, artist_id, username, json_file_path = extract_info(transformed_input_url)
-            return get_list_of_user_urls(domain, service, artist_id, transformed_input_url), username, json_file_path
+            domain, service, artist_id, username = extract_info(transformed_input_url)
+            json_data = generate_json_dictionary_from_data(transformed_input_url)
+            return get_list_of_user_urls(domain, service, artist_id, transformed_input_url), username, json_data
 
         elif user_choice == "2":
             transformed_input_url = input_and_transform_url()
