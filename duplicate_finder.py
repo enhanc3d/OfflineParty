37c3035ce9ddc71369ba49b9a0c1bf05903fa646
Eloy Gracia <eloygb2000@gmail.com>
2023-10-01 02:59:23 +0200

    Fixed "Download all" issues with user_search

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -8,26 +8,37 @@ def collect_choices(id_name_service_mapping):
     while True:
         choices = input("Which one(s) do you want to choose? (Separated by commas) ")
         try:
-            choice_list = [int(choice.strip()) for choice in choices.split(',')]
+            # Parse choices and remove duplicates
+            choice_list = list(set(int(choice.strip()) for choice in choices.split(',')))
+            
+            # Check for out-of-range choices
             invalid_choices = [choice for choice in choice_list if choice < 1 or choice > len(id_name_service_mapping) + 1]
             if invalid_choices:
                 print(f"Invalid choice(s): {', '.join(map(str, invalid_choices))}. Please choose valid options.")
                 continue
+            
+            # Check for invalid combination: both specific options and "Download All"
+            # Check for invalid combination: both specific options and "Download All"
+            if len(id_name_service_mapping) + 1 in choice_list and len(choice_list) > 1:
+                print("Invalid combination. Please select either specific options or 'Download All'.")
+                continue
+
             return choice_list
         except ValueError:
             print("Invalid input. Please enter valid numeric choices separated by commas.")
+            continue
 
 def find_and_return_entries(data_list, input_username):
     input_username = input_username.lower()
     potential_matches = []
-    
+
     for item in data_list:
         name = item.get('name', '').capitalize()
         service = item.get('service', '').capitalize()
         if name.lower() == input_username:
             artist_service = f"{name} ({service})"
             potential_matches.append((artist_service, item))
-    
+
     if not potential_matches:
         print(f"No matching entries found for {input_username.capitalize()}")
         return None
@@ -37,14 +48,16 @@ def find_and_return_entries(data_list, input_username):
         print(f"{i}. {artist_service}")
     print(f"{len(potential_matches) + 1}. Download all\n")
 
-    choice_list = collect_choices({k: v for k, v in potential_matches})
+    choice_list = collect_choices(dict(potential_matches))
 
+    # If the choice is "Download all", return a list of dictionaries instead of a list of tuples
     if len(choice_list) == 1 and choice_list[0] == len(potential_matches) + 1:
         print("Returning all entries...")
-        return potential_matches
+        return [entry[1] for entry in potential_matches]
 
-    selected_entry = potential_matches[choice_list[0] - 1][1]  # Extract the dictionary from the tuple
-    return selected_entry
+    # For specific choices, return selected entries
+    return [potential_matches[i - 1][1] for i in choice_list]
+
 
 if __name__ == "__main__":
     data_list = [
