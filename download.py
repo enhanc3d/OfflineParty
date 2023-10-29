a59f25040f650d41b98254b43d793dba888e5adc
2000GHz <59731892+2000GHz@users.noreply.github.com>
2023-10-29 20:57:00 +0100

    Better comment for disk limit

--- download.py
+++ download.py
@@ -42,7 +42,7 @@ def load_settings():
     settings = {
         'stash_path': './',
         'post_limit': 0,  # 0 downloads all posts from the artist, it's the default value
-        'disk_limit': 0,  # Placeholder value, in MB
+        'disk_limit': 0,  # 0 disables the download limit. Expressed in MB
         'download_preference' : 0 
     }
 
